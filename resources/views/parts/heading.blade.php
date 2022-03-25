e2e82bd261a08239dba49657e5b0d0301c5001e9
Solomon Muoki <somuoki977@gmail.com>
2022-03-26 00:24:17 +0300

    Heading Update

--- resources/views/parts/heading.blade.php
+++ resources/views/parts/heading.blade.php
@@ -21,7 +21,7 @@
                 <li class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 border border-white @if(Route::currentRouteName() == 'characters') bg-indigo-600  text-white @else text-gray-600 bg-gray-50 @endif cursor-pointer px-3 py-2.5 font-normal text-xs leading-3 shadow-md rounded">
                     <a href="/characters">Characters</a> </li>
             </ul>
-            <div class=" flex space-x-5 justify-center items-center pl-2">
+            <!--<div class=" flex space-x-5 justify-center items-center pl-2">
                 <div class="relative cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 ">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M21 15C21 15.5304 20.7893 16.0391 20.4142 16.4142C20.0391 16.7893 19.5304 17 19 17H7L3 21V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H19C19.5304 3 20.0391 3.21071 20.4142 3.58579C20.7893 3.96086 21 4.46957 21 5V15Z" stroke="#1F2937" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -35,7 +35,7 @@
                     <path d="M13.73 21C13.5542 21.3031 13.3019 21.5547 12.9982 21.7295C12.6946 21.9044 12.3504 21.9965 12 21.9965C11.6496 21.9965 11.3054 21.9044 11.0018 21.7295C10.6982 21.5547 10.4458 21.3031 10.27 21" stroke="#1F2937" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
             </div>
-        </nav>
+        </nav> -->
         <!-- for smaller devcies -->
 
         <div class="block md:hidden w-full mt-5 ">
